2da8057a92ba8af7702baa698d5a235405c4c0e2
Vinay <rmdort@gmail.com>
2016-09-30 14:56:14 +0800

    Added email, url, phone number extractors

--- datextractor/__init__.py
+++ datextractor/__init__.py
@@ -1,1 +1,2 @@
-from main import *
+from time import *
+from communication import *
--- example.py
+++ example.py
@@ -1,6 +1,9 @@
-from datextractor import tag, ground
-from mx.DateTime import *
+from datextractor import tag, ground, tag_phone, tag_email, tag_url
+from mx.DateTime import now
 
 # tagged_text = tag('The invoice was working on twenty five minutes ago.')
-tagged_text = tag('The invoice was working on 10 days later.')
-print ground(tagged_text, now())
+# tagged_text = tag('The invoice was working on 10 days later.')
+# print ground(tagged_text, now())
+# print tag_phone('Hello my phone number is +91 90291420')
+# print tag_email('Hello my email address is rmdort@gmail.com and hello@hotmail.c')
+# print tag_url('Hello my website url is http://www.olasearch.com')
