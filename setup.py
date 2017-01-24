a518b846bc1bac255978430e26aa81af5a0c557c
Vinay <rmdort@gmail.com>
2017-01-24 11:21:52 +0800

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+import sys
 from setuptools import setup, find_packages
 name = 'datextractor'
 version='0.0.1'
 package_dir = {name: name}
+required=['python-dateutil']
+if sys.version_info < (3, 3):
+  required.append('backports.shutil_get_terminal_size')
 setup(
   name=name,
   version=version,
   license='MIT',
-  package_dir=package_dir
+  package_dir=package_dir,
+  install_requires=required
 )
