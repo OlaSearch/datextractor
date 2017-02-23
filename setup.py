a26c2f208220206198a1a9fc1fb777a7e31057e3
Vinay <rmdort@gmail.com>
2017-02-23 14:23:06 +0800

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 from setuptools import setup, find_packages
 name = 'datextractor'
-version='0.0.1'
+version='0.0.2'
 package_dir = {name: name}
 required=['python-dateutil']
 if sys.version_info < (3, 3):
